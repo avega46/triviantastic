67867b3794024ca9321f15f3c02eaf3c0ce1510f
Alan Vega <avega46@stu.southtexascollege.edu>
2023-03-07 15:25:54 -0600

    category call

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
       <RouterView /> 
     </div>
   </main>
+  <p class="score">Score: 100</p>
 </template>
 
 <style lang="postcss" scoped>
@@ -17,5 +18,9 @@ main {
   & .card {
     @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
   }
+}
+
+.score {
+  @apply absolute bottom-6 right-8 text-2xl font-bold uppercase text-slate-700;
 }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref} from 'vue'
+import useAPI from '@/composables/useAPI'
+
+const api = useAPI()
+const  categories = ref([])
+
+onMounted(async () => {
+  categories.value = await api.getCategories()
+})
+</script>
 
 <template>
  <div class="brand">
@@ -7,7 +17,10 @@
   <img class="logo" src="logo.svg" alt="logo" />
  </div>
  <div class="categories">
-  <RouterLink :to="`/question/category/${n}`" v-for="n in 24" :key="n" class="category">Category - {{ n }}</RouterLink>
+  <RouterLink 
+  :to="`/question/category/${category.id}`" v-for="category in categories" 
+  :key="category.id" class="category"> {{ category.name }}
+  </RouterLink>
  </div>
 </template>
 
@@ -28,7 +41,7 @@
   @apply grid flex-grow grid-cols-4 gap-12;
 
   & .category {
-    @apply flex h-32 w-40 items-center justify-center rounded border-4 py-4 font-bold uppercase text-slate-500 transition-colors duration-300;
+    @apply text-center flex h-32  items-center justify-center rounded border-4 py-4 font-bold uppercase text-slate-500 transition-colors duration-300;
 
     &:hover {
       @apply cursor-pointer border-red-500 bg-red-500 text-white;
