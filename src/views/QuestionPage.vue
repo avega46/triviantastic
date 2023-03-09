89c52ebb844520abf5e4d20312da41377fcbf45e
Alan Vega <avega46@stu.southtexascollege.edu>
2023-03-09 04:32:33 -0600

    added answer logic

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,32 +1,53 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import { shuffle } from 'lodash-es'
+
+import NotificationAnswers from '@/components/NotificationAnswer.vue'
 
 import useAPI from '@/composables/useAPI'
 import useColor from '@/composables/useColor'
+import useScore from '@/composables/useScore'
 import BaseTitle from '@/components/BaseTitle.vue'
-
+import DifficultyChip from '@/components/DifficultyChip.vue'
 
 const route = useRoute()
+const router = useRouter()
 const colors = useColor()
 const api = useAPI()
 const question = ref(null)
 const answers = ref([])
+const showNotification = ref(false)
+const isCorrect = ref(false)
 
+const { changeScore } = useScore()
+const handleAnswer = (points) => {
+  isCorrect.value = points > 0
+  showNotification.value = true
+  setTimeout(() => {
+  changeScore(points)
+    router.push('/')
+  }, 1000)
+}
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
   answers.value.push({
     id: answers.value.length,
     correct: true,
     answer: question.value.correct_answer,
+    points: question.value.diffculty === 'easy' ? 10 :
+        question.value.diffculty === 'medium' ? 20 : 30,
+    
   })
   question.value.incorrect_answers.map((answer) => {
     answers.value.push({
       id: answers.value.length,
       correct: false,
       answer,
+      points : -5,
     })
   })
+  answers.value = shuffle(answers.value)
 })
 </script>
 
@@ -35,10 +56,14 @@ onMounted(async () => {
   <BaseTitle> {{ question.category }} </BaseTitle>
   <p class="question">{{ question.question }}</p>
   <div class="answers">
-    <div  v-for="answer in answers" :key="answer.id" :class="colors.getColor(answer.id)" class="answer">{{ answer.answer }}</div>
+    <div @click="handleAnswer(answer.points)" v-for="answer in answers" :key="answer.id" :class="colors.getColor(answer.id)" class="answer">
+    {{ answer.answer }}
+    </div>
   </div> 
+  <DifficultyChip :difficulty="question.difficulty"/>
   </div>
   <div v-else class="loading">Loading...</div>
+  <NotificationAnswers :correct="isCorrect" v-if="showNotification" />
 </template>
 
 <style lang="postcss" scoped>
@@ -54,6 +79,10 @@ onMounted(async () => {
 
     & .answer{
       @apply flex text-center items-center justify-center rounded-lg text-white text-4xl;
+
+      &:hover {
+        @apply cursor-pointer;
+      }
     }
   }
 }
