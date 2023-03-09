65e16abd72478f40e2098c53ff5345ad6ba07201
Alan Vega <avega46@stu.southtexascollege.edu>
2023-03-09 05:04:59 -0600

    added category once call

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,11 +3,10 @@ import { onMounted, ref} from 'vue'
 import useAPI from '@/composables/useAPI'
 import BaseTitle from "@/components/BaseTitle.vue";
 
-const api = useAPI()
-const  categories = ref([])
+const { categories, getCategories } = useAPI()
 
 onMounted(async () => {
-  categories.value = await api.getCategories()
+   await getCategories()
 })
 </script>
 
@@ -18,7 +17,7 @@ onMounted(async () => {
   </template>
   Triviantastic
  </BaseTitle>
- <div class="categories">
+ <div v-if="categories.length > 0" class="categories">
   <RouterLink 
   :to="`/question/category/${category.id}`" v-for="category in categories" 
   :key="category.id" class="category"> {{ category.name }}
